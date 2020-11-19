a95a9fbafc63626467e6786fe28f9e0dc566ca9d
Cory Potter <pottercory22@gmail.com>
2020-11-18 19:45:30 -0600

    added initial scatter chart

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,3 +1,7 @@
+// Items to compare:
+//     poverty vs obesity
+//     income vs smokes
+//     age vs healthcare
 // set svg width, height and margin
 var svgWidth = 960;
 var svgHeight = 500;
@@ -23,3 +27,93 @@ var svg = d3
 // Append an SVG group
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Initial Params
+var chosenXAxis = "poverty";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(censusData, chosenXAxis) {
+  // create scales
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(censusData, d => d[chosenXAxis]) * 0.8,
+      d3.max(censusData, d => d[chosenXAxis]) * 1.2
+    ])
+    .range([0, width]);
+
+  return xLinearScale;
+
+}
+
+// Retrieve data from the CSV file and execute everything below
+d3.csv("assets/data/data.csv").then(function(censusData, err) {
+    if (err) throw err;
+  
+    // parse data
+    censusData.forEach(function(data) {
+      data.poverty = +data.poverty;
+      data.obesity = +data.obesity;
+    });
+  
+    // xLinearScale function above csv import
+    var xLinearScale = xScale(censusData, chosenXAxis);
+  
+    // Create y scale function
+    var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(censusData, d => d.obesity)])
+      .range([height, 0]);
+  
+    // Create initial axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+  
+    // append x axis
+    var xAxis = chartGroup.append("g")
+      .classed("x-axis", true)
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+  
+    // append y axis
+    chartGroup.append("g")
+      .call(leftAxis);
+  
+    // append initial circles
+    var circlesGroup = chartGroup.selectAll("circle")
+      .data(censusData)
+      .enter()
+      .append("circle")
+      .attr("cx", d => xLinearScale(d[chosenXAxis]))
+      .attr("cy", d => yLinearScale(d.obesity))
+      .attr("r", 15)
+      .attr("fill", "#06D6A0")
+      .attr("opacity", ".5");
+  
+    // Create group for two x-axis labels
+    var labelsGroup = chartGroup.append("g")
+      .attr("transform", `translate(${width / 2}, ${height + 20})`);
+  
+    var povertyLabel = labelsGroup.append("text")
+      .attr("x", 0)
+      .attr("y", 20)
+      .attr("value", "poverty") // value to grab for event listener
+      .classed("active", true)
+      .text("In Poverty (%)");
+  
+    // var albumsLabel = labelsGroup.append("text")
+    //   .attr("x", 0)
+    //   .attr("y", 40)
+    //   .attr("value", "num_albums") // value to grab for event listener
+    //   .classed("inactive", true)
+    //   .text("# of Albums Released");
+  
+    // append y axis for obesity
+    var obesityLabel = chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .classed("axis-text", true)
+      .text("Obese (%)");
+
+}).catch(function(error) {
+    console.log(error);
+});
